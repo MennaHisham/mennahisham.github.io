5ed31f5f43279e4db8e0d4094730b3fe2711c985
Minatallah Hisham <mennahishamh@gmail.com>
2024-03-07 16:49:45 +0200

    first commit

---
+++ scripts/projects.js
@@ -0,0 +1,24 @@
+var PROJECTS = [
+  {
+    img: 'project1.png',
+    title: 'Project 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+    link: 'https://www.jordaniandude.com',
+  },
+
+  {
+    img: 'project2.png',
+    title: 'Project 2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+    link: 'https://www.jordaniandude.com',
+  },
+  {
+    img: 'project3.jpg',
+    title: 'Project 3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+    link: 'https://www.jordaniandude.com',
+  },
+];
